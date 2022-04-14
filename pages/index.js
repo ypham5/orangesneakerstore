d00264cde2cf904fb1a2989ff290758407d27e23
ypham5 <finxcat@gmail.com>
2022-04-14 17:00:57 -0600

    testing

--- pages/index.js
+++ pages/index.js
@@ -24,9 +24,9 @@ export default function Home(props) {
       <Head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="description" content="Orange - Sneakers" />
-      <title>Orange - Sneakers Marketplace</title>
+      <title>Orange - Sneakers Marketplace StoreFront</title>
       <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Tourney:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
       <PageTitle tagline="new arrivals" title="Orange - Sneakers Marketplace"/>
